10ef05b9dadd5935892c012ef4c620a80887b75c
Abdulrahman Hamad <wadaln3ma@gmail.com>
2022-01-27 16:28:12 +0200

    fix a glitch

--- pages/search.js
+++ pages/search.js
@@ -15,7 +15,7 @@ const Search = ({ results })=>{
       <Head>
         <title>{router.query.term} - Google Search</title>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300;400;500;700&display=swap" rel="stylesheet"/>
       </Head>
 
